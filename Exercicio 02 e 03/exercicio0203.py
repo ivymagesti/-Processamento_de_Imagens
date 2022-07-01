c7cbcc20fe8ec9f33dd80819015ffedc5fa83271
Ivy Magesti <oliveiraivy67@gmail.com>
2022-07-01 12:10:48 -0300

    Update exercicio0203.py

--- Exercicio 02 e 03/exercicio0203.py
+++ Exercicio 02 e 03/exercicio0203.py
@@ -9,6 +9,6 @@ cv2.imshow('Imagem Original',img)
 print (f"Altura: {img.shape[0]} pixels") 
 print (f"Largura: {img.shape[1]} pixels")
 print (f"Canais: {img.shape[2]}")
-#print (f"Tamanho: {np.size(img)} bytes") 
+print (f"Tamanho: {np.size(img)} bytes") 
  
 cv2.waitKey(0) 
